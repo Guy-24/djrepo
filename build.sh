b78421cfcccb9b7f02ae3e680bbbd2e43f001dd5
Sathapana Tinop <Bombboom24092546@gmail.com>
2026-04-30 22:53:18 +0700

    fix build.sh command not found

--- build.sh
+++ build.sh
@@ -3,6 +3,3 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py createsuperuser --no-input || echo "Superuser already exists"
-DJANGO_SUPERUSER_EMAIL
-DJANGO_SUPERUSER_USERNAME
-DJANGO_SUPERUSER_PASSWORD
