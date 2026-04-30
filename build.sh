4a9b9d0366b6f487b93539ef3dd5a95920c5b598
Sathapana Tinop <Bombboom24092546@gmail.com>
2026-04-30 22:46:19 +0700

    create Superuser

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser --no-input || echo "Superuser already exists"
+DJANGO_SUPERUSER_EMAIL
+DJANGO_SUPERUSER_USERNAME
+DJANGO_SUPERUSER_PASSWORD
